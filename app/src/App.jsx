626970204098a37dd1de0bffd552b0a81d8a549f
Christopher Angelkos <granchrisai@gmail.com>
2016-12-14 15:59:24 -0600

    Fixed minor bugs preventing dev environment to run properly
    - Added router/router.js
    - Removed import main.css from App.js

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/main.css';
+
 
 class App extends Component {
   constructor (props) {
@@ -9,7 +9,7 @@ class App extends Component {
 
   render () {
     return (
-      <div>Test</div>
+      <div>Welcome to OpenSauce!</div>
     )
   }
 };
