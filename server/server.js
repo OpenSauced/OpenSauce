2aedb833312389878fe44617eba853f0535933de
Will Schwanke <wschwanke@gmail.com>
2016-12-15 13:21:14 -0600

    Add React Structure File

--- server/server.js
+++ server/server.js
@@ -16,8 +16,6 @@ module.exports.app = app;
 // Check to see if there is a port environment variable or just use port 4040 instead
 module.exports.NODEPORT = process.env.PORT || 4040;
 
-//app.use(morgan);
-
 // Use body-parser for parsing JSON and URLencoded body data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
