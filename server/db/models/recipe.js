189242c14c7b81deb77e219820502458743d32c2
Carolyn Commons <crcommons@gmail.com>
2016-12-14 19:20:24 -0600

    Moved db to server

---
+++ server/db/models/recipe.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var RecipeSchema = new Schema({
+	title: String,
+	creator: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	ingredients: [{
+		amount: Number,
+		measurement: String,
+		ingredient_name: String
+	}],
+	directions: String,
+	likes: Number,
+	tags: [String], 
+	recipe_images: [{
+		image_data: Buffer,
+		image_name: String,
+		mimetype: String
+	}],
+	created_at: {
+		type: Date,
+		default: Date.now
+	}
+})
+
+const Recipe = mongoose.model('recipe', RecipeSchema);
+
+module.exports = Recipe;
