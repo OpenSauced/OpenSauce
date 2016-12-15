1ac265d74632b097bd515a021702100131074843
Will Schwanke <wschwanke@gmail.com>
2016-12-15 17:26:08 -0600

    Worked on this branch instead of correct one.

---
+++ app/src/Components/App/AppWidgetsR.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class AppWidgetsR extends Component {
+  constructor () {
+    super();
+  }
+
+  render () {
+    return (
+      <div>AppWidgetsR</div>
+    )
+  }
+}
+
+export default AppWidgetsR;
