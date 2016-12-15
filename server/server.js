e6e681204d22de87fc2efcfe7fc0dbc4249f9159
Bennett Staley <me@bennettstaley.com>
2016-12-14 19:52:32 -0600

    initial start. Created passport routes for testing.

--- server/server.js
+++ server/server.js
@@ -4,29 +4,76 @@ const session = require('express-session');
 const router = require('./router/router.js');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
+const passport = require('passport');
+const Strategy = require('passport-facebook').Strategy;
+const config = require('./db/config.js');
 // Use express and export it
 const app = express();
 module.exports.app = app;
-
 // Check to see if there is a port environment variable or just use port 4040 instead
 module.exports.NODEPORT = process.env.PORT || 4040;
-
-//app.use(morgan);
-
 // Use body-parser for parsing JSON and URLencoded body data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
 // User cookie-parser to parse cookies we get from Facebook
 app.use(cookieParser());
-
 // Serve the static client HTML files
 app.use(express.static(path.join(__dirname, '/../app/public')));
 // Serve the static client React files
 app.use('/dist', express.static(path.join(__dirname, '/../app/public/dist')));
 // Serve the node modules
 app.use('/lib', express.static(path.join(__dirname, '/../node_modules')));
+
+
+//passport app strategy
+passport.use(new Strategy({ //fill these in!!
+    clientID: config.fbObj.appId, //config?
+    clientSecret: config.fbObj.appSecret, //wt?ha
+    callbackURL: config.fbObj.callbackURL //missing this 2
+},
+//facebook sends back tokens and profile
+function(accessToken, refreshToken, profile, done) {
+  //in the DB?
+    db.User.findOne({fb_id: profile.id}).exec().then((data) => {
+        //console.log(data);
+        if (!data) { //did we get data back? no;
+            console.log('profile', profile)
+            new db.User({
+                userName: profile.displayName,
+                fb_id: profile.id,
+                picture: 'https://graph.facebook.com/' + profile.id + '/picture?type=normal'
+            }).save().then((data) => {}).catch((err) => {
+                console.error(err);
+            })
+        }
+    })
+    return done(null, profile);
+}));
+
+//Serialize and deserialize users out of the session.
+passport.serializeUser(function(user, done) {
+    return done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+app.use(session({secret: 'keyboard cat', resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', passport.authenticate('facebook'));
+app.get('/facebook/oauth', passport.authenticate('facebook', {failureRedirect: '/login'}), (req, res) => {
+    let cookie = {
+        session: req.sessionID,
+        userID: req.user.id
+    }
+    res.cookie('fr-session', cookie, {
+        maxAge: 9000000,
+        httpOnly: true
+    }).redirect('/');
+});
 
 // Start the actual server listening on the port variable
 app.listen(module.exports.NODEPORT, function (err) {
