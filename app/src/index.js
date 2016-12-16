7b1eaec30112a50bc01ccbbe8693f6aa700f9878
Will Schwanke <wschwanke@gmail.com>
2016-12-16 12:33:47 -0600

    Remove bundle.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory } from 'react-router';
 
 import Homepage from './Homepage';
+import AddRecipe from './AddRecipe';
 
 
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={Homepage}/>
     {
-    //<Route path="/:user/addrecipe" component={AddRecipe}/>
+      <Route path="/addrecipe" component={AddRecipe}/>
     //<Route path="/:user/:recipeId" component={ViewRecipe}/>
     }
   </Router>
