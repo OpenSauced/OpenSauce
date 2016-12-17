34ab6b66c914029a67e6c665902fea771ca3c44d
Bennett Staley <me@bennettstaley.com>
2016-12-17 14:58:50 -0600

    init user bcrypt stuffs. Deleted passport-facebook, not within scope.

--- server/server.js
+++ server/server.js
@@ -5,12 +5,10 @@ const userRoutes = require('./routes/userRoutes.js');
 const recipeRoutes = require('./routes/recipeRoutes.js');
 const db = require('./db/db.js')
 const config = require('./env/config')
-const authFn = require('./routes/authRoutes.js')
+const auth = require('./routes/authRoutes.js')
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const passport = require('passport');
-const Strategy = require('passport-facebook').Strategy;
 
 const app = express();
 module.exports.app = app;
@@ -31,52 +29,18 @@ app.use(express.static(path.join(__dirname, '/../app/public')));
 
 app.use('/dist', express.static(path.join(__dirname, '/../app/public/dist')));
 
-//passport app strategy
-passport.use(new Strategy({
-    clientID: config.fbObj.appId,
-    clientSecret: config.fbObj.appSecret,
-    callbackURL: config.fbObj.callbackURL,
-		enableProof: true,
-		profileFields: ['displayName', 'id', 'email']
-},
-//facebook sends back tokens and profile
-function(accessToken, refreshToken, profile, done) {
-    //find or create user!
-		console.log('proooo', profile)
-    db.userFunctions.createOrFindUser(profile).then((data) => {
-        //console.log(data);
-        // if (!data) { //did we get data back? no;
-        //     console.log('profile', profile)
-        // }
-    })
-    return done(null, profile);
-}));
-
-passport.serializeUser(function(user, done) {
-    return done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-    done(null, obj);
-});
-
 app.use(session({secret: 'git baked', resave: true, saveUninitialized: true}));
 
-app.use(passport.initialize());
-
-app.use(passport.session());
-
-app.get('/login', passport.authenticate('facebook'));
-app.get('/facebook/oauth', passport.authenticate('facebook', {failureRedirect: '/login'}), (req, res) => {
-    let cookie = {
-        session: req.sessionID,
-        userID: req.user.id
-    }
-    res.cookie('fr-session', cookie, {
-        maxAge: 9000000,
-        httpOnly: true
-    }).redirect('/');
-});
+// app.get('/facebook/oauth', passport.authenticate('facebook', {scope: ['email'], failureRedirect: '/login'}), (req, res) => {
+//     let cookie = {
+//         session: req.sessionID,
+//         userID: req.user.id
+//     }
+//     res.cookie('fr-session', cookie, {
+//         maxAge: 9000000,
+//         httpOnly: true
+//     }).redirect('/');
+// });
 
 // Start the actual server listening on the port variable
 app.listen(module.exports.NODEPORT, function(err) {
@@ -90,6 +54,22 @@ app.listen(module.exports.NODEPORT, function(err) {
 })
 
 //example of a locked route:
-app.get('/locked', authFn.ensureAuthenticated, function(req, res) {
+app.get('/locked', auth.ensureAuthenticated, function(req, res) {
     res.send('you are logged in!')
+})
+
+
+app.get('/signup', auth.ensureAuthenticated, function(req, res) {
+	res.sendFile(path.resolve(__dirname + '/../app/public/signup.html'));
+})
+app.get('/login', auth.ensureAuthenticated, function(req, res) {
+	res.sendFile(path.resolve(__dirname + '/../app/public/login.html'));
+})
+
+
+app.post('/login', auth.ensureAuthenticated, function(req, res) {
+    // res.send('you are logged in!')
+})
+app.post('/singup', auth.ensureAuthenticated, function(req, res) {
+    // res.send('you are logged in!')
 })
