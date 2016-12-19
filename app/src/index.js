951d33c38ee8b1c3b62d4c072221537388dadffb
Christopher Angelkos <granchrisai@gmail.com>
2016-12-19 16:48:16 -0600

    Adding containers

--- app/src/index.js
+++ app/src/index.js
@@ -9,12 +9,13 @@ import RouteHomepage from './RouteHomepage';
 import RouteProfile from './RouteProfile';
 import RouteAddRecipe from './RouteAddRecipe';
 import Route404 from './Route404';
+import SearchBar from './containers/SearchBar'
 
 //Redux Stuff
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
-
+import ReduxPromise from 'redux-promise'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -23,17 +24,24 @@ ReactDOM.render((
     <Router history={browserHistory}>
       {/* Homepage route */}
       <Route path="/" component={RouteHomepage}/>
+
       {/* Current User Profile Settings route */}
       <Route path="/profile" component={RouteProfile}/>
-      {/* Add Recipe route */}
-      <Route path="/addrecipe" component={RouteAddRecipe}/>
+      
       {/* Signup route */}
       <Route path="/signup" component={RouteSignup}/>
+      
       {/* Login route */}
       <Route path="/signup" component={RouteSignup}/>
+
+      {/* Recipe Routes */}
+      <Route path="/addrecipe" component={RouteAddRecipe}/>
+      <Route path="/search" component={SearchBar}/>
+
       {/* These routes will handle 404 errors */}
       <Route path="/*" component={Route404}/>
       <Route path="/**/*" component={Route404}/>
+      
     </Router>
   </Provider>
-),document.getElementById('root'));
+), document.getElementById('root'));
