e99b896fdf2d1a8e75082493868a215247b5f070
Will Schwanke <wschwanke@gmail.com>
2016-12-20 15:52:04 -0600

    Fixed server route not delivering index.html

--- app/src/components/AddRecipe/AddRecipe.jsx
+++ app/src/components/AddRecipe/AddRecipe.jsx
@@ -10,8 +10,16 @@ class AddRecipe extends Component {
 
   render() {
     return (
+<<<<<<< 10061c2c1c4eea7f5ebe4137b8fcb3fcb5eb4348
       <div class="row add_recipe">
         
+=======
+      <div>
+        <h1>AddRecipe</h1>
+        <AddRecipeUploadImage/>
+        <AddRecipeManualEntry/>
+        <AddRecipeSearchAPI/>
+>>>>>>> Fixed server route not delivering index.html
       </div>
     );
   }
