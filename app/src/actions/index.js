711d63cf2210e994cf6c2a2499c1b04c27162471
Christopher Angelkos <granchrisai@gmail.com>
2016-12-19 18:20:12 -0600

    Installed axios

---
+++ app/src/actions/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export const FETCH_RECIPES = 'FETCH_RECIPES'
+
+export const fetchRecipes = (data) => {
+  
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+
+  }
+}
