2131a41bbe85c20d0cf1d014956c31abf7aa562f
Will Schwanke <wschwanke@gmail.com>
2016-12-21 11:17:03 -0600

    Rebase

---
+++ app/src/components/AddRecipe/AddRecipeManual.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class AddRecipeManual extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default AddRecipeManual;
