9073e9228b5c1b0128e96eb727d22e47232405de
Christopher Angelkos <granchrisai@gmail.com>
2016-12-21 18:01:10 -0600

    added AddRecipe Page

--- app/src/containers/AddRecipe.js
+++ app/src/containers/AddRecipe.js
@@ -6,8 +6,8 @@ export default class AddRecipe extends Component {
     this.state = {
       title: '',
       description: '',
-      directions: [],
-      ingredients: []
+      directions: '',
+      ingredients: ['']
     }
   }
 
@@ -20,6 +20,9 @@ export default class AddRecipe extends Component {
     case 'recipe-description':
       this.setState({description: event.target.value})
       break
+    case 'recipe-directions':
+      this.setState({directions: event.target.value})
+      break
     }
 
     return null
@@ -27,38 +30,102 @@ export default class AddRecipe extends Component {
 
   onFormSubmit(event) {
     event.preventDefault()
-    alert('FORM SUBMISSION DETECTED')
-    //this.setState({term: ''})
-    //this.props.fetchRecipes()
+    alert('ADD AXIOS CALL TO SUBMIT STATE  HERE:')
+
+  } 
+
+  renderIngredients(item, index, collection) {
+
+    return (
+      <div>
+        <input
+          key={index}
+          id={`ingredients-${index}`}
+          onChange={this.onIngredientChange.bind(this)}
+          value={this.state.ingredients[index]}
+          placeholder="new ingredient"
+          className=""
+        />
+        <button
+          type="button"
+          key={`remove_ingredient-${index}`}
+          id={`remove_ingredient-${index}`} 
+          className=""
+          onClick={this.removeIngredient.bind(this)}
+        >
+          Remove
+        </button>
+      </div>
+    )
+  }
+
+  onIngredientChange(event) {
+    var index = event.target.id.split('-')[1]
+    var newIngredients = this.state.ingredients.slice()
+  
+    newIngredients[index] = event.target.value
+    this.setState({ingredients: newIngredients})
   }  
 
-  componentWillMount() {
+  addNewIngredient() {
+    var newIngredients = this.state.ingredients.slice()
+    newIngredients.push(event.target.value)
+    this.setState({ingredients: newIngredients})
+  }
 
+  removeIngredient() {
+    var index = event.target.id.split('-')[1]
+    var newIngredients = this.state.ingredients.slice()
+    newIngredients.splice(index, 1)
+    this.setState({ingredients: newIngredients})
   }
 
   render() {
     return(
       <form
-        className="input-group"
+        className=""
         onSubmit={this.onFormSubmit.bind(this)}
       >
         <h3>Recipe Title:</h3>
         <input
           placeholder="Please enter Recipe Name"
-          className="form-control"
+          className=""
           id="recipe-title"
           value={this.state.title}
           onChange={this.onInputChange.bind(this)}
         />
+
         <h3>Recipe Description:</h3>
         <input
           placeholder="Please enter a description"
-          className="form-control"
+          className=""
           id="recipe-description"
           value={this.state.description}
           onChange={this.onInputChange.bind(this)}
         />
-        <span className="input-group-btn">
+
+        <h3>Directions </h3>
+        <input
+          placeholder="Please enter directions"
+          className=""
+          id="recipe-directions"
+          value={this.state.directions}
+          onChange={this.onInputChange.bind(this)}
+        />
+
+        <h3>Ingredients</h3>
+        
+        {this.state.ingredients.map(this.renderIngredients.bind(this))}
+        
+        <button
+          type="button" 
+          className=""
+          onClick={this.addNewIngredient.bind(this)}
+        >
+          Add New Ingredient
+        </button>
+        
+        <span className="">
           <button type="submit" className="btn btn-secondary">
             Submit
           </button>
