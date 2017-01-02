1973f3c34babc7a8fd530c79f97fadbc8902d5de
Henry Hedges <henryhedges1@gmail.com>
2017-01-02 16:59:21 -0600

    added scraper and bug fix in AddRecipe from link

--- app/src/components/AddRecipe/AddRecipeFromLink.jsx
+++ app/src/components/AddRecipe/AddRecipeFromLink.jsx
@@ -2,38 +2,34 @@ import React, { Component } from 'react';
 import {browserHistory} from 'react-router';
 
 
-class AddRecipeFromLink extends Component {
-   constructor() {
-    super();
-  }
-
-  scrapeRecipe(e){
+const AddRecipeFromLink = (props) => {
   console.log("getting current user?", props)
+  var username = props.userData.username
+  var formSubmit = function(e) {
     e.preventDefault();
     $.ajax({
       url: '/api/recipes/scraperecipe',
       type: 'POST',
       data: {
-        username: props.userData.username,
+        username: username,
         url: $('input[name=url]').val()
       },
       success: function (recipe) {
         console.log("recipe was saved to the DB", recipe);
-
-    const path = `/viewrecipe/${recipe._id}`
-    console.log("getting current path?", path)
-      browserHistory.push(path);      
-    },
+        const path = `/viewrecipe/${recipe._id}`
+        console.log("getting current path?", path)
+        browserHistory.push(path);      
+      },
       error: function(xhr, status, err) {
           console.error(this.props.url, status, err.toString());
       }
 
-  })
-}
+    })
+  }
 
   return (
-  	<div>
-    	<form id='commentPostForm' onSubmit={formSubmit}>
+    <div>
+      <form id='commentPostForm' onSubmit={formSubmit}>
       Paste a link to a recipe:
       <input type='text' name='url' />
       <input type='submit' value='Get Recipe' />
@@ -44,5 +40,4 @@ class AddRecipeFromLink extends Component {
 };
 
 export default AddRecipeFromLink
-
 
