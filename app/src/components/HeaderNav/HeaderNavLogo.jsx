d7852a50365472a41f8c50a0955cb5002e396767
Henry Hedges <henryhedges1@gmail.com>
2017-01-03 14:33:16 -0600

    bug fix - homepage not loading when logo image is clicked on

--- app/src/components/HeaderNav/HeaderNavLogo.jsx
+++ app/src/components/HeaderNav/HeaderNavLogo.jsx
@@ -4,7 +4,9 @@ import { Router, Link } from 'react-router';
 const HeaderNavLogo = () => {
   return (
     <div className="col-xs-4">
-      <img src='/assets/icons/cookbook.svg' href='/' alt="Fork a recipe icon" height="120" width="120" />
+      <a href='/'>
+        <img src='/assets/icons/cookbook.svg' alt="Fork a recipe icon" height="120" width="120" />
+      </a>
     </div>
   );
 }
