5720003270788587a61ed3cb82a25935555db12d
Bennett Staley <me@bennettstaley.com>
2017-01-03 09:58:51 -0600

    homepage card first run. Needs cutoff text AND same height cards.

--- app/src/components/Homepage/HPFeedRecipe.jsx
+++ app/src/components/Homepage/HPFeedRecipe.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 
 const HPFeedRecipe = ({recipe}) => {
   return (
-    <li className="col-xs-12 col-sm-4 recipe_card-cont">
-      <div className="recipe_card-box">
+    <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+    <li className="card recipe">
+      <img className="card-img-top" src="/assets/tempRecipe.svg" alt="Card image cap"/>
+      <div className="card-block">
         <img src="" alt="" title="" className="recipe_card-image"/>
-        <h2 className="recipe_card-title"><a href={`/viewrecipe/${recipe._id}`}>{recipe.title}</a></h2>
-        <p className="recipe_card-desc">{recipe.description}</p>
+        <h2 className="card-title"><a href={`/viewrecipe/${recipe._id}`}>{recipe.title}</a></h2>
+        <p className="card-text">{recipe.description}</p>
         <div className="row flex-items-xs-center recipe_card-fork_like-cont">
-          <a href={`/addrecipe?recipe=${recipe._id}`} className="recipe_card-fork_button col-xs-4">Fork Recipe</a>
+          <a href={`/addrecipe?recipe=${recipe._id}`} className="btn btn-primary">Fork Recipe</a>
           <a className="recipe_card-like_button col-xs-4">Like Recipe</a>
         </div>
       </div>
     </li>
+  </div>
   );
 }
 
