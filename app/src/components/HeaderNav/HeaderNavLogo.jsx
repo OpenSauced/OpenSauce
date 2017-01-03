ea8db05ae9e05bc7993da49b36e8e5d393bad34c
Henry Hedges <henryhedges1@gmail.com>
2017-01-03 14:38:10 -0600

    added react router functionality to logo/homepage navigation bug fix

--- app/src/components/HeaderNav/HeaderNavLogo.jsx
+++ app/src/components/HeaderNav/HeaderNavLogo.jsx
@@ -4,9 +4,14 @@ import { Router, Link } from 'react-router';
 const HeaderNavLogo = () => {
   return (
     <div className="col-xs-4">
-      <a href='/'>
-        <img src='/assets/icons/cookbook.svg' alt="Fork a recipe icon" height="120" width="120" />
-      </a>
+      <Link to={'/'}>
+        <img 
+          src='/assets/icons/cookbook.svg' 
+          alt="Fork a recipe icon" 
+          height="120" 
+          width="120" 
+        />
+      </Link>  
     </div>
   );
 }
