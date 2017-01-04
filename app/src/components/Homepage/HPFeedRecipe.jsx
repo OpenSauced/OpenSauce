afda4c6697a10234f1946366a12bcdbb5e723bb4
Henry Hedges <henryhedges1@gmail.com>
2017-01-04 11:00:28 -0600

    added lazy load to homepage

--- app/src/components/Homepage/HPFeedRecipe.jsx
+++ app/src/components/Homepage/HPFeedRecipe.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 
+// LazyLoad allows the component to be loaded as  the user scrolls instead of all at once
+import LazyLoad from 'react-lazy-load';
+
 const HPFeedRecipe = ({recipe}) => {
   return (
     <div className="card-group col-xs-12 col-sm-6 col-md-4 col-lg-3">
-    <li className="card recipe">
-      <img className="card-img-top" src="/assets/tempRecipe.svg" alt="Card image cap"/>
-      <div className="card-block">
-        <div className="row flex-items-xs-center recipe_card-fork_like-cont" style={{'margin-bottom':'1.25rem'}}>
-          <a href={`/addrecipe?recipe=${recipe._id}`} className="btn btn-primary recipeCardBtn">Fork Recipe</a>
-          <a className="btn btn-secondary recipe_card-like_button recipeCardBtn">Like Recipe</a>
-        </div>
-        <h2 className="card-title"><a href={`/viewrecipe/${recipe._id}`}>{recipe.title}</a></h2>
-        <p className="card-text">{recipe.description}</p>
-        <p> {'score' in recipe ? "Search Score: " + recipe.score : ''}</p>
-      </div>
-    </li>
-  </div>
+      {/* see LazyLoad docs for more offset options (that is really all you can change)*/}
+      <LazyLoad height={425} offset={100} >
+        <li className="card recipe">
+          <img className="card-img-top" src="/assets/tempRecipe.svg" alt="Card image cap"/>
+          <div className="card-block">
+            <div className="row flex-items-xs-center recipe_card-fork_like-cont" style={{'margin-bottom':'1.25rem'}}>
+              <a href={`/addrecipe?recipe=${recipe._id}`} className="btn btn-primary recipeCardBtn">Fork Recipe</a>
+              <a className="btn btn-secondary recipe_card-like_button recipeCardBtn">Like Recipe</a>
+            </div>
+            <h2 className="card-title"><a href={`/viewrecipe/${recipe._id}`}>{recipe.title}</a></h2>
+            <p className="card-text">{recipe.description}</p>
+          </div>
+        </li>
+      </LazyLoad>
+    </div>
   );
 }
 
