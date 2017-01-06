f7a2aaba7e1e0493f16268338c7da31bdb86f060
Bennett Staley <me@bennettstaley.com>
2017-01-06 14:49:55 -0600

    mongod is mandatory. All hail.

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 db.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.connection.on('open', function() {
-   console.log('Mongdb connection open');
+   console.log('All hail the mongod!');
 })
 
 app.use('/api/users', userRoutes);
