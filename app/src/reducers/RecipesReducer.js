f337fbe29909f153a26badf2aee8c59d41064cda
Christopher Angelkos <granchrisai@gmail.com>
2017-01-07 11:39:11 -0600

    removed additional console logs

--- app/src/reducers/RecipesReducer.js
+++ app/src/reducers/RecipesReducer.js
@@ -7,9 +7,6 @@ export default (state = null, action) => {
       return action.payload.data
 
     case GET_USER_RECIPES:
-      //SAVED RECIPES ARE DUPLICATES
-      //TO LIKE AND UNLIKE HERE MAKE DB CALLS THAT WILL RETURN THE USERS RECIPES POPULATED CHANGE THE LINK THAT FIRES SO THAT IT WILL KNOW WHAT TO DO AT ACTION LEVEL OR REDUCER LEVEL
-      console.log('TODO: CHECK FOR DUPLICATES',action.payload.data.my_recipes.concat(action.payload.data.saved_recipes))
       return action.payload.data.my_recipes.concat(action.payload.data.saved_recipes)
     
     case CLEAR_RECIPES:
