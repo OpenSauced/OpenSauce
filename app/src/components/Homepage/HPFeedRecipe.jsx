8b471c323c22c5e09c2a8ca1182a169403f5de9e
Christopher Angelkos <granchrisai@gmail.com>
2017-01-06 20:52:03 -0600

    Enabled Like Feature in Recipe List

--- app/src/components/Homepage/HPFeedRecipe.jsx
+++ app/src/components/Homepage/HPFeedRecipe.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 // LazyLoad allows the component to be loaded as  the user scrolls instead of all at once
 import LazyLoad from 'react-lazy-load';
 
-const HPFeedRecipe = ({recipe}) => {
+const HPFeedRecipe = ({recipe, savedRecipes, addRecipe, removeRecipe, userId, recipeId}) => {
+  console.log(recipe._id)
   return (
-
     <LazyLoad offset={100} className="card-group col-xs-12 col-sm-6 col-md-4 col-lg-3">
       {/* see LazyLoad docs for more offset options (that is really all you can change)*/}
         <div className="card recipe">
@@ -19,7 +19,19 @@ const HPFeedRecipe = ({recipe}) => {
               <a 
                 href={`/addrecipe?recipe=${recipe._id}`} 
                 className="btn btn-primary recipeCardBtn">Fork Recipe</a>
-              <a className="btn btn-secondary recipe_card-like_button recipeCardBtn">Like Recipe</a>
+              {
+                recipe._id in savedRecipes ?
+                  <div 
+                    className="btn btn-secondary recipe_card-like_button recipeCardBtn"
+                    onClick={() => {removeRecipe(recipeId, userId)}}
+                  >Unlike Recipe</div>
+                : <div 
+                    className="btn btn-secondary recipe_card-like_button recipeCardBtn"
+                    onClick={() => {addRecipe(recipeId, userId)}}
+                  >Like Recipe</div>
+              }
+
+              
             </div>
             <h2 className="card-title"><a href={`/viewrecipe?recipeId=${recipe._id}`}>{recipe.title}</a></h2>
             <p className="card-text">{recipe.description}</p>
