ce74465f1cb9b8a9ca788a844010f895fdbd42c2
Henry Hedges <henryhedges1@gmail.com>
2017-01-10 13:18:47 -0600

    altered edit recipe button on viewrecipe.js, link now spans the whole button

--- app/src/components/ViewRecipe/ViewRecipe.jsx
+++ app/src/components/ViewRecipe/ViewRecipe.jsx
@@ -11,7 +11,7 @@ const ViewRecipe = ({recipe, user}) => {
   var credit = recipe.credit ? <div>This fantastic recipe comes from: {recipe.credit}</div> : ''
   var image = recipe.recipe_images.public_url ? <div><img src={recipe.recipe_images.public_url} /></div> : '' 
 
-	if (recipe){
+  if (recipe){
     return (
       <div className="container">
         <div className="row view-recipe-container">
@@ -28,32 +28,34 @@ const ViewRecipe = ({recipe, user}) => {
               </div>
              </div>
           </div>
-          <div className="col">{image}</div>
+          <div className="col">
+            {image}
+          </div>
         </div>
         <div className="row justify-content-between view-recipe-container">
-        <div className="col-8"> 
-        <h3>Ingredients:</h3>
-          <RecipeIngredientsList recipeIngredients={recipe.ingredients}/>
+          <div className="col-8"> 
+          <h3>Ingredients:</h3>
+            <RecipeIngredientsList recipeIngredients={recipe.ingredients}/>
           </div>
-        <div className="col-2">
-          { 
-            user._id === recipe.creator._id
-            ? <button className="btn btn-primary"> <Link to={`/editrecipe?recipeId=${recipe._id}`}>Edit this recipizzle</Link> </button>
-            : ( <div>
-                  <h5> Want to put your own spin on this recipe? Try forking it! </h5>
-                </div>
-              )
-          }
+          <div className="col-2">
+            { 
+              user._id === recipe.creator._id
+              ? <button className="btn btn-primary"> <Link to={`/editrecipe?recipeId=${recipe._id}`}>Edit this recipizzle</Link> </button>
+              : ( <div>
+                    <h5> Want to put your own spin on this recipe? Try forking it! </h5>
+                  </div>
+                )
+            }
           </div>  
-          </div> 
-          <div className="row view-recipe-container"> 
+        </div> 
+        <div className="row view-recipe-container"> 
           <h3>Directions:</h3>
           <RecipeDirections recipeDirections={recipe.directions}/>
           <div>
-          <br></br>
-          {credit}
+            <br></br>
+            {credit}
           </div>  
-          </div>  
+        </div>  
       </div>
     );
   }
