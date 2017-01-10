2f28ed7899ed3e88a0088ad52f94c2cfc5cba044
Carolyn Commons <crcommons@gmail.com>
2017-01-10 13:49:18 -0600

    Added fonts and basic homepage styling

--- app/src/components/ViewRecipe/RecipeCreator.jsx
+++ app/src/components/ViewRecipe/RecipeCreator.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const RecipeCreator = ({recipeCreator}) => {
 	return (
-  <div>{recipeCreator.username + ' cooked up this recipe.'}</div>
+  <div className="col-12 creator" >{recipeCreator.username + ' cooked up this recipe.'}</div>
   )
 };
 
--- app/src/components/ViewRecipe/SaveAndForkButtons.jsx
+++ app/src/components/ViewRecipe/SaveAndForkButtons.jsx
@@ -3,20 +3,18 @@ import { Link } from 'react-router';
 
 const SaveAndForkButtons = ({recipeId, userId, saveRecipe, forkRecipe, removeRecipe, savedRecipes}) => {
   var star = recipeId in savedRecipes ? 
-    <div className ="col" onClick={() => removeRecipe(recipeId, userId)}>
-    <i className="fa fa-star fa-3x icon"></i><br></br><div className="icon">Remove this Recipe from My Cookbook</div></div>
-  : <div className ="col" onClick={() => saveRecipe(recipeId, userId)}>
-    <i className="fa fa-star fa-3x icon"></i><br></br><div className="icon">Add this Recipe to My Cookbook</div></div>  
+    <div className ="col-2 icon" onClick={() => removeRecipe(recipeId, userId)}>
+    <i className="fa fa-star fa-3x"></i><br></br><div className="icon">Remove this Recipe from My Cookbook</div></div>
+  : <div className ="col-2 icon" onClick={() => saveRecipe(recipeId, userId)}>
+    <i className="fa fa-star fa-3x"></i><br></br><div className="icon">Add this Recipe to My Cookbook</div></div>  
   
   return (
-    <div className="container">
-    <div className="row"> 
+    <div className="row justify-content-center"> 
     {star}
-    <Link className="col" to={`/addrecipe?recipe=${recipeId}`}>
+    <Link className="col-2 icon" to={`/addrecipe?recipe=${recipeId}`}>
     <i className="fa fa-cutlery fa-3x icon"></i>
     <br></br>
     <p className="icon">Fork this Recipe</p></Link> 
-    </div>
     </div>
   );
 };
