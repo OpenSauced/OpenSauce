669b54d0ff6dc69e8e306914ec335de95f457117
Carolyn Commons <crcommons@gmail.com>
2017-01-09 20:07:16 -0600

    scrolling transition for menu/navbar

--- app/src/containers/App.js
+++ app/src/containers/App.js
@@ -4,9 +4,25 @@ import '../assets/styles/main.scss';
 import '../assets/styles/recipe_card.scss';
 
 export default class App extends Component {
-  // componentWillMount() {
+  componentDidMount() {
 
-  // }
+  	$(window).scroll(function() {
+  		console.log("this!!!!!!!!! ", $(this).scrollTop())
+    if ($(this).scrollTop() > 400) {
+    	console.log("WE DID IT!")
+    	console.log($('.navbar'))
+        $('.navbar').attr('style',  'background-color:rgb(60, 114, 80)');
+    }
+    if ($(this).scrollTop() < 400) {
+    	console.log("WE DID IT!")
+    	console.log($('.navbar'))
+        $('.navbar').attr('style',  'background-color:rgba(60, 114, 80, 0.8)');
+    }
+	});
+  	 
+  }
+
+
   render() {
     return (
       <div>
