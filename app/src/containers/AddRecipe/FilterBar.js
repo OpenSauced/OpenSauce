661fd4e4f5855052b89f35070c53e716973f56bd
Christopher Angelkos <granchrisai@gmail.com>
2017-01-09 20:14:34 -0600

    Added FilterBar Component

---
+++ app/src/containers/AddRecipe/FilterBar.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+import { browserHistory } from 'react-router'
+import _ from 'lodash'
+
+class FilterBar extends Component {
+  constructor(props) {
+    super(props)
+    this.search = _.debounce((isSubmit, filter) => {this.sendSearch(isSubmit, filter)}, 300)
+  }
+
+  onInputChange(event) {
+    this.props.updateSearchTerm(event.target.value)
+    this.search(false)
+    this.state = {
+      forkedRecipes: true,
+      savedRecipes: true,
+      myRecipes: true
+    }
+    return null
+  }
+
+  sendSearch(isSubmit, filter) {
+
+    var location = browserHistory.getCurrentLocation()
+    //console.log(location)
+    if (isSubmit) {
+      var url = location.pathname + (this.props.searchTerm ? '?term=' + this.props.searchTerm : '')
+      browserHistory.push(url)
+
+    } else {
+      this.props.clearRecipes()
+      switch (location.pathname) {
+        case '/':
+          var searchstring = this.props.searchTerm ? '?term=' + this.props.searchTerm : location.search
+          this.props.fetchRecipes(searchstring, filter)
+          break
+        case '/myrecipes':
+          console.log('TODO: MAKE SEARCHBAR WORK IN MYRECIPES') 
+      }
+    }
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault()
+    this.search(true)
+  }
+
+  onCheckboxChange(event) {
+    this.search(false, event.target.id)
+  }
+
+
+  renderFilters() {
+    return (
+      <div>
+      <h3> now viewing: </h3>
+        <label><input
+          id="saved_recipes_checkbox" 
+          onChange={this.onCheckboxChange.bind(this)} 
+          type="checkbox" defaultChecked="true"
+        />SAVED RECIPES</label>  &nbsp; &nbsp; 
+        <label><input
+          id="forked_recipes_checkbox"  
+          onChange={this.onCheckboxChange.bind(this)} 
+          type="checkbox" defaultChecked="true"
+        />FORKED RECIPES</label> &nbsp; &nbsp; 
+        <label><input 
+          id="my_recipes_checkbox" 
+          onChange={this.onCheckboxChange.bind(this)} 
+          type="checkbox" 
+          defaultChecked="true"
+        />LIKED RECIPES</label>
+      </div>
+    )
+  }
+
+  render() {
+    //console.log(this.props.searchTerm)
+    return (
+      <div className="d-flex justify-content-center">
+      <form
+        className="col-8"
+        onSubmit={this.onFormSubmit.bind(this)}
+      >
+        <input
+          placeholder="Search for recipes..."
+          className="form-control"
+          id="searchfield"
+          value={this.props.searchTerm}
+          onChange={this.onInputChange.bind(this)}
+        />
+        {this.renderFilters()}
+      </form>
+      </div>
+    )
+  }
+}
+
+//REDUX STUFF
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchRecipes, updateSearchTerm, clearRecipes } from '../../actions/index'
+
+const  mapStateToProps = (state) => {
+  return {
+    recipes: state.recipes,
+    searchTerm: state.searchTerm
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators ({ fetchRecipes, updateSearchTerm, clearRecipes }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar)
