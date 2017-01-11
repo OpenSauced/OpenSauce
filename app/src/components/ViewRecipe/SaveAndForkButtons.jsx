3c076f86bfa88cc85bc2c0de1c530fd61dd4ff51
Carolyn Commons <crcommons@gmail.com>
2017-01-10 19:39:40 -0600

    fixed the bug on the footer and stuff

--- app/src/components/ViewRecipe/SaveAndForkButtons.jsx
+++ app/src/components/ViewRecipe/SaveAndForkButtons.jsx
@@ -4,17 +4,16 @@ import { Link } from 'react-router';
 const SaveAndForkButtons = ({recipeId, userId, saveRecipe, forkRecipe, removeRecipe, savedRecipes}) => {
   var star = recipeId in savedRecipes ? 
     <div className ="col-2 icon" onClick={() => removeRecipe(recipeId, userId)}>
-    <i className="fa fa-star fa-3x"></i><br></br><div className="icon">Remove this Recipe from My Cookbook</div></div>
+    <i className="fa fa-star fa-3x"><br></br><p>Remove this Recipe</p></i></div>
   : <div className ="col-2 icon" onClick={() => saveRecipe(recipeId, userId)}>
-    <i className="fa fa-star fa-3x"></i><br></br><div className="icon">Add this Recipe to My Cookbook</div></div>  
+    <i className="fa fa-star fa-3x"><br></br><p>Save this Recipe</p></i></div>  
   
   return (
     <div className="row justify-content-center"> 
     {star}
     <Link className="col-2 icon" to={`/addrecipe?recipe=${recipeId}`}>
-    <i className="fa fa-cutlery fa-3x icon"></i>
-    <br></br>
-    <p className="icon">Fork this Recipe</p></Link> 
+    <i className="fa fa-cutlery fa-3x icon"><br></br><p>Fork this Recipe</p></i>
+    </Link> 
     </div>
   );
 };
