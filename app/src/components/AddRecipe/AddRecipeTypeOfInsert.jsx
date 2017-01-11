ba16d93714538a5b8bebae23e5710e1d1c2e3213
Carolyn Commons <crcommons@gmail.com>
2017-01-10 19:00:47 -0600

    adjusting the layout of the get recipe from link page

--- app/src/components/AddRecipe/AddRecipeTypeOfInsert.jsx
+++ app/src/components/AddRecipe/AddRecipeTypeOfInsert.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const AddRecipeTypeOfInsert = ({renderClick}) => {
   return (
     <div className="row justify-content-center">
-      <div className="col-2"><button className="btn btn-secondary" name="manual" onClick={renderClick}>Type New Recipe</button></div>
-      <div className="col-2"><button className="btn btn-secondary" name="link" onClick={renderClick}>Get Recipe from Other Site</button></div>
+      <div className="col-2"><button className="btn btn-primary" name="manual" onClick={renderClick}>Add Your Own Recipe</button></div>
+      <div className="col-2"><button className="btn btn-primary" name="link" onClick={renderClick}>Get A Recipe From A Link</button></div>
     </div>
   );
 }
