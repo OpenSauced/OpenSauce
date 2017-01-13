80ae3b0e85dd9c5a17863a3263f466308ab2c9b4
Henry Hedges <henryhedges1@gmail.com>
2017-01-13 02:27:12 -0600

    Revert "151 tree view"

--- app/src/components/ViewRecipe/RecipeTree.jsx
+++ app/src/components/ViewRecipe/RecipeTree.jsx
@@ -11,31 +11,61 @@ import SortableTree from 'react-sortable-tree';
 class RecipeList extends Component {
   constructor () {
     super ()
+    this.state = {
+      treeData: {}
+    }
   }
-
   disableDrag() {
     window.requestAnimationFrame(() => {
       [].slice.call(document.getElementsByClassName('rst__moveHandle')).forEach(element => {element.remove()})
     })
   }
 
+  componentDidMount() {
+    this.disableDrag()
+    this.setTreeData()
+  }
+  setTreeData() {
+    var currentRecipe = this.props.currentRecipe
+
+    var treeData = [
+        { 
+          title: 'MOMS SPAGGETHI', 
+          expanded: true,
+          subtitle: (<a href="#">John Rambo</a>) ,
+          children: [ 
+            { title: 'SISTERS SPAGGETHI' }, 
+            { title: currentRecipe.title,
+              subtitle: currentRecipe.creator.username,
+              expanded: true,
+              children: [
+                {title: 'SONS SPAGGETHI'},
+                {title: 'DAUGHTERS SPAGGETTH'}
+               ]
+            }
+          ]
+        }
+    ]
+    this.setState({treeData: treeData})
+  }
+  
   render() {
-    //console.log(this.props.treeData)
+    console.log(this.props.currentRecipe)
     return (
       <SortableTree 
-        treeData={this.props.treeData}
-        isVirtualized={false} 
-        onChange={() => {}}
-        ref={() => {this.disableDrag();window.scrollTo(0, 0)}}
+              treeData={this.state.treeData}
+              isVirtualized={false} 
+              onChange={() => {}}
       />
-      
     )
   }
 }
 
 const  mapStateToProps = (state) => {
   return {
-    treeData: state.treeData
+    recipes: state.recipes,
+    userData: state.userData,
+    currentRecipe: state.currentRecipe
   }
 }
 
--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -4,15 +4,13 @@ import UserReducer from './UserReducer'
 import SearchTermReducer from './SearchTermReducer'
 import CurrentRecipeReducer from './CurrentRecipeReducer'
 import OffsetReducer from './OffsetReducer'
-import TreeDataReducer from './TreeDataReducer'
 
 const rootReducer = combineReducers({
   recipes: RecipesReducer,
   userData: UserReducer,
   searchTerm: SearchTermReducer,
   currentRecipe: CurrentRecipeReducer,
-  offset: OffsetReducer,
-  treeData: TreeDataReducer,
+  offset: OffsetReducer
 });
 
 export default rootReducer;
