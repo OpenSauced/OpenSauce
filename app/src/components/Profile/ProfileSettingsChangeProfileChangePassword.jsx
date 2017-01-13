e05d27b71d0155cb8c39d302282abd670cc6651a
Will Schwanke <wschwanke@gmail.com>
2017-01-13 11:56:43 -0600

    Finish responsive design

--- app/src/components/Profile/ProfileSettingsChangeProfileChangePassword.jsx
+++ app/src/components/Profile/ProfileSettingsChangeProfileChangePassword.jsx
@@ -11,9 +11,9 @@ class ChangePassword extends Component {
                 <h2 className="col-12">Change your Password</h2>
                 <form className="col-12 row" action="/api/users/updateInfo/password" method="post" encType="multipart/form-data" target="_top">
                     <label className="col-12 col-form-label" htmlFor="password">Old Password:</label>
-                    <div className="col-5"><input className="form-control" type="text" name="password"/></div>
+                    <div className="col-12 col-md-5"><input className="form-control" type="text" name="password"/></div>
                     <label className="col-12 col-form-label" htmlFor="newPassword">New Password:</label>
-                    <div className="col-5"><input className="form-control" type="text" name="newPassword"/></div>
+                    <div className="col-12 col-md-5"><input className="form-control" type="text" name="newPassword"/></div>
                     <div className="col-12">
                         <input className="btn btn-secondary" type="submit" value="Save"/>
                     </div>
