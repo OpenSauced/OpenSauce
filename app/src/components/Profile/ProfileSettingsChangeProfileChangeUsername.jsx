d3f0897b08f219b2d0bd288675ebece6cffdeb5f
Will Schwanke <wschwanke@gmail.com>
2017-01-13 11:50:52 -0600

    Profile css changes

--- app/src/components/Profile/ProfileSettingsChangeProfileChangeUsername.jsx
+++ app/src/components/Profile/ProfileSettingsChangeProfileChangeUsername.jsx
@@ -11,9 +11,9 @@ class ChangeUsername extends Component {
                 <h2 className="col-12"> Current User: {this.props.data} </h2>
                 <form className="col-12 row basicProfileSettingsForm" action="/api/users/updateInfo/username" method="post" encType="multipart/form-data" target="_top">
                     <label className="col-12 col-form-label" htmlFor="newUsername">Username:</label>
-                    <div className="col-5"><input className="form-control" type="text" name="newUsername"/></div>
+                    <div className="col-12 col-md-5"><input className="form-control" type="text" name="newUsername"/></div>
                     <label className="col-12  col-form-label" htmlFor="password">Password:</label>
-                    <div className="col-5"><input className="form-control" type="text" name="password"/></div>
+                    <div className="col-12 col-md-5"><input className="form-control" type="text" name="password"/></div>
                     <div className="col-12">
                         <input className="btn btn-secondary" type="submit" value="Save"/>
                     </div>
--- app/src/components/Profile/ProfileSettingsChangeProfileName.jsx
+++ app/src/components/Profile/ProfileSettingsChangeProfileName.jsx
@@ -11,11 +11,11 @@ class ChangeName extends Component {
                 <h2 className="col-12"> Current name: {this.props.data.first_name + ' ' + this.props.data.last_name} </h2>
                 <form className="col-12 row" action="/api/users/updateInfo/name" method="post" encType="multipart/form-data" target="_top">
                     <label className="col-12 col-form-label" htmlFor="firstName">First Name:</label>
-                    <div className="col-5"><input className="form-control" type="text" name="firstName" required/></div>
+                    <div className="col-12 col-md-5"><input className="form-control" type="text" name="firstName" required/></div>
                     <label className="col-12 col-form-label" htmlFor="lastName">Last Name:</label>
-                    <div className="col-5"><input className="form-control" type="text" name="lastName" required/></div>
+                    <div className="col-12 col-md-5"><input className="form-control" type="text" name="lastName" required/></div>
                     <label className="col-12 col-form-label" htmlFor="password">Password:</label>
-                    <div className="col-5"><input className="form-control" type="text" name="password"/></div>
+                    <div className="col-12 col-md-5"><input className="form-control" type="text" name="password"/></div>
                     <div className="col-12">
                         <input className="btn btn-secondary" type="submit" value="Save"/>
                     </div>
