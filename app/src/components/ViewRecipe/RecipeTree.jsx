d46f59b4ccfaf3ae23b50a4fab7fba65414f516e
Christopher Angelkos <granchrisai@gmail.com>
2017-01-12 19:34:40 -0600

    Bound to database fixing sync issues

--- app/src/components/ViewRecipe/RecipeTree.jsx
+++ app/src/components/ViewRecipe/RecipeTree.jsx
@@ -20,42 +20,87 @@ class RecipeList extends Component {
       [].slice.call(document.getElementsByClassName('rst__moveHandle')).forEach(element => {element.remove()})
     })
   }
+  componentWillMount() {
+    this.setTreeData()
+  }
 
   componentDidMount() {
     this.disableDrag()
-    this.setTreeData()
+   
   }
   setTreeData() {
     var currentRecipe = this.props.currentRecipe
+    var treeData = []
+    //     { 
+    //       title: 'MOMS SPAGGETHI', 
+    //       expanded: true,
+    //       subtitle: (<a href="#">John Rambo</a>) ,
+    //       children: [ 
+    //         { title: 'SISTERS SPAGGETHI' }, 
+    //         { title: currentRecipe.title,
+    //           subtitle: currentRecipe.creator.username,
+    //           expanded: true,
+    //           children: [
+    //             {title: 'SONS SPAGGETHI'},
+    //             {title: 'DAUGHTERS SPAGGETTH'}
+    //            ]
+    //         }
+    //       ]
+    //     }
+    // ]
 
-    var treeData = [
-        { 
-          title: 'MOMS SPAGGETHI', 
-          expanded: true,
-          subtitle: (<a href="#">John Rambo</a>) ,
-          children: [ 
-            { title: 'SISTERS SPAGGETHI' }, 
-            { title: currentRecipe.title,
-              subtitle: currentRecipe.creator.username,
-              expanded: true,
-              children: [
-                {title: 'SONS SPAGGETHI'},
-                {title: 'DAUGHTERS SPAGGETTH'}
-               ]
-            }
-          ]
-        }
-    ]
+    var currentNode = []
+
+    //push currentRecipeInfo to the tree
+
+    currentNode.push({title: currentRecipe.title, expanded: true})
+    treeData = currentNode
+    
+    //check for children
+    if (currentRecipe.forked_children.length) {
+      currentNode[0].children = []
+      currentRecipe.forked_children.forEach(child => {
+        currentNode[0].children.push(
+          {
+            title: child.title,
+            subtitle: child.creator.username
+          }
+        )
+      })
+    }
+
+    //check for parrent node
+    var parentNode = {}
+    if (currentRecipe.forked_parent) {
+      parentNode.title = currentRecipe.forked_parent.title
+      parentNode.subtitle = currentRecipe.forked_parent.creator.username
+      parentNode.expanded = true
+      parentNode.children = currentNode
+
+      //check for siblings
+      if (currentRecipe.forked_parent.forked_children.length > 1) {
+        currentRecipe.forked_parent.forked_children.forEach(child => {
+          if (child._id !== currentRecipe._id) currentNode.unshift(
+            {title: child.title, subtitle: child.creator.username}
+          ) 
+        })
+      }
+
+      treeData = [parentNode]
+    }
+
+    
     this.setState({treeData: treeData})
+  
   }
   
   render() {
     console.log(this.props.currentRecipe)
     return (
       <SortableTree 
-              treeData={this.state.treeData}
-              isVirtualized={false} 
-              onChange={() => {}}
+        treeData={this.state.treeData}
+        isVirtualized={false} 
+        onChange={() => {}}
       />
     )
   }
