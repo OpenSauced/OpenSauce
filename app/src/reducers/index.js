e747d9606981bbcd477ad73f70b7811e402b4e00
Henry Hedges <henryhedges1@gmail.com>
2017-01-13 03:07:26 -0600

    Revert "Revert "151 tree view""

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -4,13 +4,15 @@ import UserReducer from './UserReducer'
 import SearchTermReducer from './SearchTermReducer'
 import CurrentRecipeReducer from './CurrentRecipeReducer'
 import OffsetReducer from './OffsetReducer'
+import TreeDataReducer from './TreeDataReducer'
 
 const rootReducer = combineReducers({
   recipes: RecipesReducer,
   userData: UserReducer,
   searchTerm: SearchTermReducer,
   currentRecipe: CurrentRecipeReducer,
-  offset: OffsetReducer
+  offset: OffsetReducer,
+  treeData: TreeDataReducer,
 });
 
 export default rootReducer;
